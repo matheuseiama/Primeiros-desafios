0c75048199cc6d65396905e1b7c1c3402e57fa75
matheuseiama <109082723+matheuseiama@users.noreply.github.com>
2022-07-14 18:15:46 -0300

    Add files via upload

---
+++ desafio_area.js
@@ -0,0 +1,9 @@
+const PI = 3.141592;
+const raio = 10;
+let areaCirc = PI * raio * raio;
+
+console.log("O resultado da area é: " + areaCirc + " m²");
+
+areaCirc = Math.PI * raio * raio;
+ 
+console.log("O resultado da area é: " + areaCirc + " m²");
---
+++ desafio_troca.js
@@ -0,0 +1,14 @@
+let a = 7;
+let b = 94; 
+let temp = a;
+
+console.log("PRIMEIRA PARTE!!!");
+console.log("valor a= " + a);
+console.log("Valor b= " + b);
+
+a = b;
+b = temp;
+
+console.log("SEGUNDA PARTE!!!");
+console.log("Valor a= " + a);
+console.log("Valor b= " + b);
